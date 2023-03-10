70a3058cc5000ba7ae338074faf298c9ee718bef
Swastik Mohapatra Incture <swastik.mohapatra@incture.com>
2023-03-10 12:33:14 +0530

    table added

--- src/pages/Inbox/Inbox.jsx
+++ src/pages/Inbox/Inbox.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./Inbox.css";
+// import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import TableInbox from "./TableInbox";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 
 const Inbox = () => {
   return (
-    <div>Inbox</div>
-  )
-}
+    <div className="wrap-cont">
+      <Container sx={{  margin: "10px"}}>
+        <Grid container spacing={2} sx={{ marginTop: "30px" }}>
+          <Grid item xs={8}>
+            <h3>
+              <u>Updates</u>
+            </h3>
+          </Grid>
 
-export default Inbox
+          <Grid item xs={4} style={{ textAlign: "right" }}>
+            <button className="addReq">
+              <AddOutlinedIcon sx={{ marginRight: "10px" }} />
+              <span>Add Request</span>
+            </button>
+          </Grid>
+          <Grid item xs={12}>
+            <TableInbox />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default Inbox;
